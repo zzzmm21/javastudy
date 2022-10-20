a817f1d4f78b0bc029d5ad7b2f1abede47d213bc
zzzmm21 <zzzmm21@gmail.com>
2022-10-20 10:59:42 +0900

    firs commit

---
+++ src/main/java/chapter1/Ex01Variable.java
@@ -0,0 +1,13 @@
+package chapter1;
+
+public class Ex01Variable {
+
+	public static void main(String[] args) {
+		int i = 1000;
+		String s = "Hello World";
+		
+		System.out.println(i);
+		System.out.println(s);
+	}
+
+}
