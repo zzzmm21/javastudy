7057aeaba2781c2414e5c0ae59aba5a5f9b508e3
zzzmm21 <zzzmm21@gmail.com>
2022-10-26 13:48:43 +0900

    다형성 - 오버로딩

--- chapter03/src/main/java/paint/Main.java
+++ chapter03/src/main/java/paint/Main.java
@@ -4,11 +4,14 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		Point pt = new Point();
+		Point pt = new Point(10, 50);
 		pt.setX(10);
 		pt.setY(50);
 		
 		drawPoint(pt);
+		//pt.disappear();
+		pt.show(false);
+	
 	}
 	public static void drawPoint(Point pt) {
 		pt.show();
--- chapter03/src/main/java/paint/Point.java
+++ chapter03/src/main/java/paint/Point.java
@@ -3,6 +3,11 @@ package paint;
 public class Point {
 	private int x;
 	private int y;
+	
+	public Point(int x ,int y) {
+		this.x = x;
+		this.y= y;
+	}
 	public int getX() {
 		return x;
 	}
@@ -15,10 +20,23 @@ public class Point {
 	public void setY(int y) {
 		this.y = y;
 	}
+	public void show(boolean visible) {
+		if(visible) {
+			show();
+		}else {
+			System.out.println("점(x=" + x + ", y=" +  y +")를 지웠습니다");
+		}
+	}
 	public void show() {
-		System.out.println("점(x=" + x + ",y=" +  y +"를 그렸습니다)");
+		System.out.println("점(x=" + x + ", y=" +  y +")를 그렸습니다");
 		
 	}
+	
+	
+	/*
+	 * public void disappear() { System.out.println("점(x=" + x + ", y=" + y
+	 * +")를 지웠습니다"); }
+	 */
 	
 
 }
