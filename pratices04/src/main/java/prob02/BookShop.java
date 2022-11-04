6b017eb51d3540f19f3d0fcb4a0fa1453796e229
zzzmm21 <zzzmm21@gmail.com>
2022-11-04 11:00:55 +0900

    upload

--- pratices04/src/main/java/prob02/BookShop.java
+++ pratices04/src/main/java/prob02/BookShop.java
@@ -18,9 +18,13 @@ public class BookShop {
 		books[9] = new Book(10, "풀하우스", "원수연");
 
 		Scanner scanner = new Scanner(System.in);
+		System.out.println("*****도서 정보 출력하기******");
+		displayBookInfo(books);
 		System.out.print("대여 하고 싶은 책의 번호를 입력하세요:");
+		
 		int num = scanner.nextInt();
 		scanner.close();
+		
 
 		// (1) 입력된 번호에 맞는 책을 찾아 대여 되었음(상태코드=0)을 체크 합니다.
 		for (Book book : books) {
